73220a73a1164a54cc34df0b9b61fb480bb836fb
TheK321 <thek321xd@gmail.com>
2022-03-16 21:49:57 -0600

    Listo para entregar

--- app/src/main/java/com/isc/apptapiz/AcercaActivity.kt
+++ app/src/main/java/com/isc/apptapiz/AcercaActivity.kt
@@ -1,11 +1,19 @@
 package com.isc.apptapiz
 
+import android.media.Image
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class AcercaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.acercade)
+
+        val info : ImageView = findViewById(R.id.info)
+        info.setOnClickListener{
+            Toast.makeText(applicationContext,"Aplicación para cambiar fondos de pantalla",Toast.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/java/com/isc/apptapiz/MainActivity.kt
+++ app/src/main/java/com/isc/apptapiz/MainActivity.kt
@@ -7,45 +7,46 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
 
-    var movil = 0
+    var movil : Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val iv1: ImageView = findViewById(R.id.imagen1)
         val iv2: ImageView = findViewById(R.id.imagen2)
         val iv3: ImageView = findViewById(R.id.imagen3)
         val iv4: ImageView = findViewById(R.id.imagen4)
-        val boton: Button = findViewById(R.id.button1)
         val btnAcerca: Button = findViewById(R.id.button2)
-
+        val principal:ImageView = findViewById(R.id.principal)
+        chooseImage(R.drawable.fondo)
         iv1.setOnClickListener{
-            chooseImage(R.drawable.imagina)
+            chooseImage(R.drawable.fondo)
         };
 
         iv2.setOnClickListener {
-            chooseImage(R.drawable.imaginab)
+            chooseImage(R.drawable.fondo2)
         };
 
         iv3.setOnClickListener {
-            chooseImage(R.drawable.imaginac)
+            chooseImage(R.drawable.fondo3)
         }
         iv4.setOnClickListener {
-            chooseImage(R.drawable.imaginad)
+            chooseImage(R.drawable.fondo5)
         }
 
-        boton.setOnClickListener{
+        principal.setOnClickListener{
             val fondo = BitmapFactory.decodeStream(
                 resources.openRawResource(movil)
             )
             try {
+                Toast.makeText(applicationContext,"Se ha cambiado el fondo de pantalla exitosamente",Toast.LENGTH_LONG).show()
                 WallpaperManager.getInstance(this).setBitmap(fondo)
             } catch (e: IOException) {
                 e.printStackTrace()
